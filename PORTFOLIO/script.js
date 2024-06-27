7302dd226fd77dedbea3f57d3fcc9ff105d10066
nikitaag21 <116826899+nikitaag21@users.noreply.github.com>
2024-06-27 12:22:07 +0530

    Add files via upload

---
+++ PORTFOLIO/script.js
@@ -0,0 +1,14 @@
+console.log("Nikita's Portfolio");
+var tablinks = document.getElementsByClassName("title1");
+var tabcontents = document.getElementsByClassName("tab-contents");
+ 
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link");
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab");
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
